48aeef7f2009e83bb82d4ed733e4a664934f93e8
Michael Neely <michael@michaelneely.net>
2020-06-23 23:37:19 -0400

    Added nodemon, request and express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-hello
+var express = require('express')
+var app = express()
+var request = require('request')
+
+app.get('/', function(req, res) {
+    res.send('Hello World')
+})
+
+app.listen(3000)
